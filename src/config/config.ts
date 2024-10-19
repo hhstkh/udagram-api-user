60516207d15f2db7783bef843a8416c493393cc6
Si <sihuynh.tkh@gmail.com>
2024-10-20 00:49:16 +0700

    init user api

---
+++ src/config/config.ts
@@ -0,0 +1,14 @@
+export const config = {
+  'username': 'postgres',
+  'password': 'Matkhau123',
+  'database': 'imagedb',
+  'host': 'imagedb.cp6q6g8cizva.us-east-1.rds.amazonaws.com',
+  'dialect': 'postgres',
+  'aws_region': 'us-east-1',
+  'aws_profile': 'default',
+  'aws_media_bucket': 'arn:aws:s3:::test-238088665613-dev',
+  'url': 'http://localhost:8100',
+  'jwt': {
+    'secret': 'testing',
+  },
+};
